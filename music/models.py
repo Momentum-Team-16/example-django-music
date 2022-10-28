b397ad6be5c9145b7dc5d045f4f652027f37efe1
Amy Gori <amydgori@gmail.com>
2022-10-28 19:30:35 -0400

    Add models for Album and Artist

--- music/models.py
+++ music/models.py
@@ -6,3 +6,41 @@ class User(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(db_index=True, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.ForeignKey(
+        "Artist", on_delete=models.SET_NULL, null=True, blank=True
+    )
+    release_date = models.DateField(blank=True, null=True)
+    favorited_by = models.ManyToManyField(User, related_name="favorite_albums")
+
+    def __repr__(self):
+        return f"<Album {self.title} pk={self.pk} >"
+
+    def __str__(self):
+        return self.title
+
+
+class Artist(BaseModel):
+    INDIVIDUAL = "IND"
+    GROUP = "GRP"
+
+    ARTIST_TYPE_CHOICES = ((INDIVIDUAL, "individual"), (GROUP, "group"))
+
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=3, choices=ARTIST_TYPE_CHOICES)
+
+    def __repr__(self):
+        return f"<Artist {self.name} pk={self.pk}>"
+
+    def __str__(self):
+        return self.name
